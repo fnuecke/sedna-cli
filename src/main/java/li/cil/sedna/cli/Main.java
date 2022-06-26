fc1f3af93ba59a389c181d2fff13100de621c918
Florian Nücke <florian@nuecke.de>
2022-06-26 19:17:53 +0200

    Update to new Sedna version with GDB support.

--- src/main/java/li/cil/sedna/cli/Main.java
+++ src/main/java/li/cil/sedna/cli/Main.java
@@ -6,7 +6,6 @@ import li.cil.sedna.api.device.BlockDevice;
 import li.cil.sedna.api.device.PhysicalMemory;
 import li.cil.sedna.buildroot.Buildroot;
 import li.cil.sedna.device.block.ByteBufferBlockDevice;
-import li.cil.sedna.device.block.SparseBlockDevice;
 import li.cil.sedna.device.memory.Memory;
 import li.cil.sedna.device.rtc.GoldfishRTC;
 import li.cil.sedna.device.rtc.SystemTimeRealTimeCounter;
@@ -23,35 +22,39 @@ import org.jline.terminal.impl.DumbTerminal;
 import org.jline.utils.NonBlockingReader;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
 public final class Main {
     public static void main(final String[] args) throws Exception {
         Sedna.initialize();
 
-        for (final String arg : args) {
-            if ("--benchmark".equals(arg)) {
-                runBenchmark();
-                return;
-            } else if ("--terminal".equals(arg)) {
-                runTerminal();
-                return;
-            }
-        }
+        final List<String> argList = Arrays.asList(args);
+        final boolean enableGdbStub = argList.contains("-s");
+        final boolean waitForGdb = argList.contains("-S");
+        final boolean runBenchmark = argList.contains("--benchmark");
+        final boolean terminalMode = argList.contains("--terminal");
 
-        runSimple();
+        if (runBenchmark) {
+            runBenchmark();
+        } else if (terminalMode) {
+            runTerminal(enableGdbStub, waitForGdb);
+        } else {
+            runSimple(enableGdbStub, waitForGdb);
+        }
     }
 
-    private static void runSimple() throws Exception {
+    private static void runSimple(final boolean enableGdbStub, final boolean waitForGdb) throws Exception {
         final R5Board board = new R5Board();
-        final PhysicalMemory memory = Memory.create(20 * 1024 * 1024);
+        final PhysicalMemory memory = Memory.create(32 * 1024 * 1024);
         final UART16550A uart = new UART16550A();
         final GoldfishRTC rtc = new GoldfishRTC(SystemTimeRealTimeCounter.get());
 
-        final BlockDevice rootfs = ByteBufferBlockDevice.createFromStream(Buildroot.getRootFilesystem(), true);
-        final BlockDevice sparsefs = new SparseBlockDevice(rootfs);
-        final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(), sparsefs);
-        final VirtIOFileSystemDevice fs = new VirtIOFileSystemDevice(board.getMemoryMap(),
-                "host_fs", new HostFileSystem());
+        final Images images = getImages();
+
+        final BlockDevice rootfs = ByteBufferBlockDevice.createFromStream(images.rootfs(), false);
+        final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(), rootfs);
+        final VirtIOFileSystemDevice fs = new VirtIOFileSystemDevice(board.getMemoryMap(), "host_fs", new HostFileSystem());
 
         uart.getInterrupt().set(0xA, board.getInterruptController());
         rtc.getInterrupt().set(0xB, board.getInterruptController());
@@ -69,11 +72,15 @@ public final class Main {
 
         board.reset();
 
-        loadProgramFile(memory, Buildroot.getFirmware());
-        loadProgramFile(memory, Buildroot.getLinuxImage(), 0x200000);
+        loadProgramFile(memory, images.firmware());
+        loadProgramFile(memory, images.kernel(), 0x200000);
 
         board.initialize();
         board.setRunning(true);
+
+        if (enableGdbStub) {
+            board.enableGDB(55554, waitForGdb);
+        }
 
         final int cyclesPerSecond = board.getCpu().getFrequency();
         final int cyclesPerStep = 1_000;
@@ -101,8 +108,8 @@ public final class Main {
                 }
 
                 if (board.isRestarting()) {
-                    loadProgramFile(memory, Buildroot.getFirmware());
-                    loadProgramFile(memory, Buildroot.getLinuxImage(), 0x200000);
+                    loadProgramFile(memory, images.firmware());
+                    loadProgramFile(memory, images.kernel(), 0x200000);
 
                     board.initialize();
                 }
@@ -117,96 +124,102 @@ public final class Main {
         }
     }
 
-    private static void runTerminal() throws Exception {
-        final Terminal terminal = TerminalBuilder.builder().jansi(false).jna(true).build();
-        if (terminal.getClass() == DumbTerminal.class) {
-            return;
-        }
-
-        final NonBlockingReader reader = terminal.reader();
-        final PrintWriter writer = terminal.writer();
-
-        if (!terminal.getSize().equals(new Size(80, 25))) {
-            try {
-                terminal.setSize(new Size(80, 25));
-            } catch (final UnsupportedOperationException e) {
-                System.err.println("Cannot resize window to 80x25. Things might look a bit derpy.");
+    private static void runTerminal(final boolean enableGdbStub, final boolean waitForGdb) throws Exception {
+        try (final Terminal terminal = TerminalBuilder.builder().jansi(false).jna(true).build()) {
+            if (terminal.getClass() == DumbTerminal.class) {
+                runSimple(enableGdbStub, waitForGdb);
+                return;
             }
-        }
 
-        final R5Board board = new R5Board();
-        final PhysicalMemory memory = Memory.create(20 * 1024 * 1024);
-        final UART16550A uart = new UART16550A();
-        final GoldfishRTC rtc = new GoldfishRTC(SystemTimeRealTimeCounter.get());
-        final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(),
-                ByteBufferBlockDevice.createFromStream(Buildroot.getRootFilesystem(), true));
+            final NonBlockingReader reader = terminal.reader();
+            final PrintWriter writer = terminal.writer();
 
-        uart.getInterrupt().set(0xA, board.getInterruptController());
-        rtc.getInterrupt().set(0xB, board.getInterruptController());
-        hdd.getInterrupt().set(0x1, board.getInterruptController());
-
-        board.addDevice(0x80000000L, memory);
-        board.addDevice(uart);
-        board.addDevice(rtc);
-        board.addDevice(hdd);
-
-        board.setBootArguments("root=/dev/vda ro");
-        board.setStandardOutputDevice(uart);
-
-        board.reset();
-
-        loadProgramFile(memory, Buildroot.getFirmware());
-        loadProgramFile(memory, Buildroot.getLinuxImage(), 0x200000);
-
-        board.initialize();
-        board.setRunning(true);
-
-        final int cyclesPerSecond = board.getCpu().getFrequency();
-        final int cyclesPerStep = 1_000;
-
-        int remaining = 0;
-        while (board.isRunning()) {
-            final long stepStart = System.currentTimeMillis();
-
-            remaining += cyclesPerSecond;
-            while (remaining > 0) {
-                board.step(cyclesPerStep);
-                remaining -= cyclesPerStep;
-
-                int value;
-                while ((value = uart.read()) != -1) {
-                    writer.append((char) value);
-                }
-                writer.flush();
-
-                while (reader.available() > 0 && uart.canPutByte()) {
-                    uart.putByte((byte) reader.read());
+            if (!terminal.getSize().equals(new Size(80, 25))) {
+                try {
+                    terminal.setSize(new Size(80, 25));
+                } catch (final UnsupportedOperationException e) {
+                    System.err.println("Cannot resize window to 80x25. Things might look a bit derpy.");
                 }
             }
 
-            if (board.isRestarting()) {
-                loadProgramFile(memory, Buildroot.getFirmware());
-                loadProgramFile(memory, Buildroot.getLinuxImage(), 0x200000);
+            final Images images = getImages();
 
-                board.initialize();
-            }
+            final R5Board board = new R5Board();
+            final PhysicalMemory memory = Memory.create(20 * 1024 * 1024);
+            final UART16550A uart = new UART16550A();
+            final GoldfishRTC rtc = new GoldfishRTC(SystemTimeRealTimeCounter.get());
+            final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(),
+                    ByteBufferBlockDevice.createFromStream(images.rootfs(), true));
 
-            final long stepDuration = System.currentTimeMillis() - stepStart;
-            final long sleep = 1000 - stepDuration;
-            if (sleep > 0) {
-                //noinspection BusyWait
-                Thread.sleep(sleep);
+            uart.getInterrupt().set(0xA, board.getInterruptController());
+            rtc.getInterrupt().set(0xB, board.getInterruptController());
+            hdd.getInterrupt().set(0x1, board.getInterruptController());
+
+            board.addDevice(0x80000000L, memory);
+            board.addDevice(uart);
+            board.addDevice(rtc);
+            board.addDevice(hdd);
+
+            board.setBootArguments("root=/dev/vda ro");
+            board.setStandardOutputDevice(uart);
+
+            board.reset();
+
+            loadProgramFile(memory, images.firmware());
+            loadProgramFile(memory, images.kernel(), 0x200000);
+
+            board.initialize();
+            board.setRunning(true);
+
+            final int cyclesPerSecond = board.getCpu().getFrequency();
+            final int cyclesPerStep = 1_000;
+
+            int remaining = 0;
+            while (board.isRunning()) {
+                final long stepStart = System.currentTimeMillis();
+
+                remaining += cyclesPerSecond;
+                while (remaining > 0) {
+                    board.step(cyclesPerStep);
+                    remaining -= cyclesPerStep;
+
+                    int value;
+                    while ((value = uart.read()) != -1) {
+                        writer.append((char) value);
+                    }
+                    writer.flush();
+
+                    while (reader.available() > 0 && uart.canPutByte()) {
+                        uart.putByte((byte) reader.read());
+                    }
+                }
+
+                if (board.isRestarting()) {
+                    loadProgramFile(memory, images.firmware());
+                    loadProgramFile(memory, images.kernel(), 0x200000);
+
+                    board.initialize();
+                }
+
+                final long stepDuration = System.currentTimeMillis() - stepStart;
+                final long sleep = 1000 - stepDuration;
+                if (sleep > 0) {
+                    //noinspection BusyWait
+                    Thread.sleep(sleep);
+                }
             }
         }
     }
 
     private static void runBenchmark() throws Exception {
+        final Images images = getImages();
+
         final R5Board board = new R5Board();
         final PhysicalMemory memory = Memory.create(20 * 1024 * 1024);
         final UART16550A uart = new UART16550A();
         final GoldfishRTC rtc = new GoldfishRTC(SystemTimeRealTimeCounter.get());
         final VirtIOBlockDevice hdd = new VirtIOBlockDevice(board.getMemoryMap(),
-                ByteBufferBlockDevice.createFromStream(Buildroot.getRootFilesystem(), true));
+                ByteBufferBlockDevice.createFromStream(images.rootfs(), true));
 
         uart.getInterrupt().set(0xA, board.getInterruptController());
         rtc.getInterrupt().set(0xB, board.getInterruptController());
@@ -245,8 +258,8 @@ public final class Main {
                 memory.store(offset, 0, Sizes.SIZE_32_LOG2);
             }
 
-            loadProgramFile(memory, Buildroot.getFirmware());
-            loadProgramFile(memory, Buildroot.getLinuxImage(), 0x200000);
+            loadProgramFile(memory, images.firmware());
+            loadProgramFile(memory, images.kernel(), 0x200000);
 
             board.initialize();
             board.setRunning(true);
@@ -301,4 +314,13 @@ public final class Main {
             memory.store(address, (byte) value, Sizes.SIZE_8_LOG2);
         }
     }
+
+    private static Images getImages() throws IOException {
+        return new Images(
+                Buildroot.getFirmware(),
+                Buildroot.getLinuxImage(),
+                Buildroot.getRootFilesystem());
+    }
+
+    private record Images(InputStream firmware, InputStream kernel, InputStream rootfs) { }
 }
